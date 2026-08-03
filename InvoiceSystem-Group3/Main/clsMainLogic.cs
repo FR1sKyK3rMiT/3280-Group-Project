bbd93b4cca7c3c1b1d675f0980b044c0cc6bc1ea
19cjames12 <156262956+19cjames12@users.noreply.github.com>
2026-08-03 14:44:45 -0600

    Add clsMainLogic with requiered interface comments

---
+++ InvoiceSystem-Group3/Main/clsMainLogic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace InvoiceSystem_Group3.Main
+{
+    public class clsMainLogic
+    {
+        public void LoadInvoiceFromSearch(string sInvoiceID)
+        {
+            //TODO
+        }
+
+        public void RefreshItemsAFterEdit()
+        {
+            //TODO
+        }
+
+        public void StartNewInvoice()
+        {
+            //TODO
+        }
+
+        public decimal CalculateTotal(decimal currentTotal, decimal itemCost, bool adding)
+        {
+            if(adding)
+                return currentTotal + itemCost;
+            else
+                return currentTotal - itemCost;
+        }
+
+        public void SwitchToEditMode()
+        {
+            //TODO
+        }
+
+
+    }
+}
+
+
